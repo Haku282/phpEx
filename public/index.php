c964793f315318809c160f6f69f152134fae973d
Haku282 <vohoquochuy2802@gmail.com>
2026-03-22 10:44:04 +0700

    bai 7

--- public/index.php
+++ public/index.php
@@ -1,12 +1,27 @@
 <?php
 // public/index.php 
+session_start();
 // Nạp file autoload của Composer 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Vohoq\Bai01QuanlySv\Controllers\SinhvienController;
+use Vohoq\Bai01QuanlySv\Controllers\UserController;
 // Simple Router 
 $action = $_GET['action'] ?? 'index';
-$controller = new SinhvienController();
+// Danh sách các action không yêu cầu đăng nhập
+$public_actions = ['login', 'register', 'do_login', 'do_register'];
+// Khởi tạo controller dựa trên action
+if (in_array($action, [
+    'login',
+    'register',
+    'do_login',
+    'do_register',
+    'logout'
+])) {
+    $controller = new UserController();
+} else {
+    $controller = new SinhvienController();
+}
 switch ($action) {
     case 'add':
         $controller->add();
@@ -21,8 +36,25 @@ switch ($action) {
     case 'delete':
         $controller->delete();
         break;
+    // Các action của UserController
+    case 'login':
+        $controller->showLoginForm();
+        break;
+    case 'do_login':
+        $controller->login();
+        break;
+    case 'register':
+        $controller->showRegisterForm();
+        break;
+    case 'do_register':
+        $controller->register();
+        break;
+    case 'logout':
+        $controller->logout();
+        break;
     case 'index':
     default:
+        $controller = new SinhvienController();
         $controller->index();
         break;
 }
